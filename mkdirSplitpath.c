391e0d0a78b56354e379b9de38840f87a0a6e58f
MarkArdrey <mark_ardrey@mines.edu>
2025-03-30 11:02:06 -0600

    Update mkdirSplitpath.c
    Pushed all edits made to the file here. mkdirSplitpath.c should be functioning as intended now.

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -1,3 +1,12 @@
+/*/
+ *   Mark Ardrey
+ *   CSCI210 :: Systems Programming 
+ *   Tolga Can
+ *   Program to split the path fed into it into a base name, and directory path.
+ *   The second part allocates memory to build a directory for use.
+ *   Intended to be used with preprogrammed touch, rm, rmdir, ls, cd, pwd commands
+/*/
+
 #include "types.h"
 
 extern struct NODE* root;
@@ -6,24 +15,169 @@ extern struct NODE* cwd;
 //make directory
 void mkdir(char pathName[]){
 
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+    //if the provided path name is "/" then there is no valid path
+    if(strcmp(pathName, "/") == 0)
+    {
+        printf("MKDIR ERROR: no path provided\n");
+        return;
+    }
 
-    printf("TO BE IMPLEMENTED\n");
+    char baseName[64];
+    char dirName[256];
 
+    struct NODE* parent = splitPath(pathName, baseName, dirName);
+    if (parent == NULL)
+    {
+        return;
+    }
+
+    struct NODE* temp = parent->childPtr;
+    int exists = 0;
+    while (temp != NULL)
+    {
+        if(strcmp(temp->name, baseName) == 0 && temp->fileType == 'D')
+        {
+            exists = 1;
+            break;
+        }
+        temp = temp->siblingPtr;
+    }
+
+    //prints error messages for incorrect inputs
+    if (exists)
+    {
+        if (dirName[0] == '/' && dirName[1] == '\0')
+        {
+            printf("MKDIR ERROR: directory %s already exists\n", baseName);
+        }
+        else if (dirName[0] == '\0') 
+        {
+            printf("MKDIR ERROR: directory %s already exists\n", baseName);
+        }
+        else
+        {
+            printf("MKDIR ERROR: directory %s/%s already exists\n", dirName, baseName);
+        }
+    }
+
+    //initializes new directory node using malloc
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    if (newNode == NULL) //sentinel for errors
+    {
+        perror("malloc");
+        exit(1);
+    }
+
+    //builds links to other nodes for the new directory
+    strncpy(newNode->name, baseName, 63);
+    newNode->name[63] = '\0';
+    newNode->fileType = 'D';
+    newNode->childPtr = NULL;
+    newNode->siblingPtr = NULL;
+    newNode->parentPtr = parent;
+
+    //creates new node as the last child
+    if (parent->childPtr == NULL)
+    {
+        parent->childPtr = newNode;
+    }
+    else
+    {
+        struct NODE* initNode = parent->childPtr;
+        while(initNode->siblingPtr != NULL)
+        {
+            initNode = initNode->siblingPtr;
+        }
+        initNode->siblingPtr = newNode;
+    }
+
+    //if the function works successfully, this message will be printed
+    printf("MKDIR SUCCESS: node %s successfully created\n", pathName);
     return;
 }
 
 //handles tokenizing and absolute/relative pathing options
 struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
-    // TO BE IMPLEMENTED
-    // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
-    // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
-    // SEE THE PROVIDED SOLUTION EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
+    char pathTemp[256];
+    char dirTemp[256];
 
-    // YOUR CODE HERE
-    //
-    return NULL;
+    strcpy(pathTemp, pathName);
+
+    //locates the end of dirName
+    int slashIndex = -1;
+    for (int i = 0; pathTemp[i] != '\0'; i++)
+    {
+        if (pathTemp[i] == '/')
+        {
+            slashIndex = i;
+        }
+    }
+
+    //splits the path into baseName and dirName, depending on what index the slash was found in
+    if (slashIndex != -1)
+    {
+        //if no path to the current directory exists
+        if (slashIndex == 0)
+        {
+            strcpy(dirName, "/");
+            strcpy(baseName, pathTemp + 1);
+        }
+        //standard operation to copy pathTemp into dirTemp, baseName, and dirName
+        //what is most common to happen
+        else 
+        {
+            for (int i = 0; i < slashIndex; i++)
+            {
+                dirTemp[i] = pathTemp[i];
+            }
+            dirTemp[slashIndex] = '\0';
+            strcpy(dirName, dirTemp);
+            strcpy(baseName, pathTemp + slashIndex + 1);
+        }
+    }
+    //handles edge case of no dirName, and only pathTemp exists
+    else
+    {
+        dirName[0] = '\0';
+        strcpy(baseName, pathTemp);
+    }
+
+    //finds the pointer to the parent directory of baseName
+    struct NODE* currentNode = (pathName[0] == '/') ? root : cwd;
+    if(strlen(dirName) == 0)
+    {
+        return currentNode;
+    }
+
+    //prepares dirName for tokenization
+    char dirCopy[256];
+    strcpy(dirCopy, dirName);
+    if (strcmp(dirCopy, "/") == 0)
+    {
+        return root;
+    }
+
+    //tokenizes directory path using '/' delimiter 
+    char* currentDirName = strtok(dirCopy, "/");
+    while(currentDirName != NULL)
+    {
+        struct NODE* child = currentNode->childPtr;
+        while(child != NULL)
+        {
+            if (strcmp(child->name, currentDirName) != 0)
+            {
+                child = child->siblingPtr;
+            }
+        }
+        if (child == NULL)
+        {
+            printf("ERROR: directory %s does not exist\n", currentDirName);
+            return NULL;
+        }
+        currentNode = child;
+        currentDirName = strtok(NULL, "/");
+    }
+
+    return currentNode;
 }
